1723dd9341c6d7e473d75fa7c44a4c045fd16345
unknown <sandeep.bachala142@gmail.com>
2020-12-29 18:07:49 -0600

    Addeed a profitle view path + Thymeleaf

--- src/main/java/io/sandeep/resumeportal/HomeController.java
+++ src/main/java/io/sandeep/resumeportal/HomeController.java
@@ -1,10 +1,13 @@
 package io.sandeep.resumeportal;
 
 import io.sandeep.resumeportal.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -19,6 +22,12 @@ public class HomeController {
     public String edit(){
         User u = new User();
         return "Edit Page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model){
+        model.addAttribute("userId",userId);
+        return "profile";
     }
 
 
